3356adcd2dc109850fd208a1290de38aff1054d0
Kshitij <singh_kshitij@yahoo.com>
2019-05-11 14:43:00 +0530

    Updates

---
+++ precache-manifest.51696229ddc35094609cf4d0c4084aa6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3894623170d5163e8ba8ee824ac848f9",
+    "url": "/kanbang/index.html"
+  },
+  {
+    "revision": "823acb67f47ecdfcbd1c",
+    "url": "/kanbang/static/css/main.c440f118.chunk.css"
+  },
+  {
+    "revision": "509d8a93e9921bcb1fab",
+    "url": "/kanbang/static/js/2.19b733ec.chunk.js"
+  },
+  {
+    "revision": "823acb67f47ecdfcbd1c",
+    "url": "/kanbang/static/js/main.269a8123.chunk.js"
+  },
+  {
+    "revision": "ebceff5b7c994dd5d9e3",
+    "url": "/kanbang/static/js/runtime~main.e3d6d278.js"
+  }
+]);
