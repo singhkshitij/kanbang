e3ea7f72a011086b70e88634383171dd7a49daec
Kshitij <singh_kshitij@yahoo.com>
2019-05-25 14:08:08 +0530

    Removed Details

--- src/components/auth/firebaseInit.jsx
+++ src/components/auth/firebaseInit.jsx
@@ -1,12 +1,12 @@
 import Firebase from 'firebase';
 
 const firebaseInit = Firebase.initializeApp({
-  apiKey: 'AIzaSyAPah9fgXnPwpBirt2WUER-qO7XevS19X4',
-  authDomain: 'scrum-board-39e8f.firebaseapp.com',
-  databaseURL: 'https://scrum-board-39e8f.firebaseio.com',
-  projectId: 'scrum-board-39e8f',
-  storageBucket: 'scrum-board-39e8f.appspot.com',
-  messagingSenderId: '909585498611'
+  apiKey: '<ENTER YOU CRED>',
+  authDomain: '<ENTER YOU CRED>',
+  databaseURL: '<ENTER YOU CRED>',
+  projectId: '<ENTER YOU CRED>',
+  storageBucket: '<ENTER YOU CRED>',
+  messagingSenderId: '<ENTER YOU CRED>'
 });
 
 export default firebaseInit;
